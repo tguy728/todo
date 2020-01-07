3cd719c443bcae8360720a5f026220416a540fcc
Trey Guy <tguy728@gmail.com>
2020-01-07 05:37:30 +0000

    fixed radial not showing issue

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-  RSpec.describe TasksController, type: :controller do
   describe "tasks#index" do
     it "should list the tasks in the database" do
       task1 = FactoryBot.create(:task)
